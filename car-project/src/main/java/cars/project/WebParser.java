20db987cd7035db44de7f8a5c3c153a670541bd5
Matthew Tzou <sonic55099@gmail.com>
2021-01-05 18:43:45 -0500

    added whitespace handling for makes and models containing whitespace

--- car-project/src/main/java/cars/project/WebParser.java
+++ car-project/src/main/java/cars/project/WebParser.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -17,12 +16,19 @@ public class WebParser {
     Scanner keyboard = new Scanner(System.in);
 
     public void searchData() {
-        String year = keyboard.next();
+        String year = keyboard.nextLine();
         year = year.trim();
-        String make = keyboard.next();
+        String make = keyboard.nextLine();
+        if (make.trim().contains(" ") == true) {
+            make = make.replace(" ", "%20");
+        }
         make = "/make/" + make.trim();
-        String model = keyboard.next();
+        String model = keyboard.nextLine();
+        if (model.trim().contains(" ") == true) {
+            model = model.replace(" ", "%20");
+        }
         model = "/model/" + model.trim() + "?format=json";
+        // Forms valid url to search NHTSA for given model year, make, and model
         String searchUrl = "https://webapi.nhtsa.gov/api/Complaints/vehicle/modelyear/" + year + make + model;
         System.out.println(searchUrl);
         try {
@@ -31,15 +37,14 @@ public class WebParser {
             InputStreamReader reader = new InputStreamReader(is);
             JsonElement je = JsonParser.parseReader(reader);
             JsonObject root = je.getAsJsonObject();
+            // Obtains primitive type at Json category "Count" containing number of complaints
             JsonPrimitive complaints = root.getAsJsonPrimitive("Count");
             System.out.println("Number of Complaints: " + complaints);
-
         } catch (MalformedURLException mue) {
             System.out.println("Error: URL invalid.");
         } catch (IOException ioe) {
             System.out.println("ioe exception");
         }
-
     } // searchData
 
     public static void main(String[] args) {
